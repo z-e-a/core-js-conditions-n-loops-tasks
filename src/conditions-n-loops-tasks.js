4f1e2c51eacaf47e445ce436d1c4b146c4954d88
z-e-a <e.a.zubkov@yandex.ru>
2024-01-18 12:29:55 +0300

    feat: solve task 'getMaxNumber'

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -38,8 +38,10 @@ function isPositive(number) {
  *  -5, 0, 5      => 5
  *  -0.1, 0, 0.2  => 0.2
  */
-function getMaxNumber(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getMaxNumber(a, b, c) {
+  if (a > b && a > c) return a;
+  if (b > c) return b;
+  return c;
 }
 
 /**
