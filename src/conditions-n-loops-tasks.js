a151b21229ec5a00e04bd9c36615617f3081b916
z-e-a <e.a.zubkov@yandex.ru>
2024-01-19 16:16:42 +0300

    feat: solve task 'isPalindrome'

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -205,8 +205,12 @@ function convertNumberToString(numberStr) {
  *  '0123210'   => true
  *  'qweqwe'    => false
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  let result = true;
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] !== str[str.length - i - 1]) result = false;
+  }
+  return result;
 }
 
 /**
