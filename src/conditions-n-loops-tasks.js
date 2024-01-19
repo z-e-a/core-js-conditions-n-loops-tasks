48596cc5033af2e48d9512a961c61e4636293fe2
z-e-a <e.a.zubkov@yandex.ru>
2024-01-19 11:42:32 +0300

    feat: solve task 'isIsoscelesTriangle'

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -87,8 +87,8 @@ function canQueenCaptureKing(queen, king) {
  *  2, 2, 5   => false
  *  3, 0, 3   => false
  */
-function isIsoscelesTriangle(/* a, b, c */) {
-  throw new Error('Not implemented');
+function isIsoscelesTriangle(a, b, c) {
+  return (a === b || a === c || b === c) && a + b > c && a + c > b && b + c > a;
 }
 
 /**
