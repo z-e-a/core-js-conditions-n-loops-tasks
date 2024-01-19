6e7b3f7ce5243b5646c329428fbcf5f9a503e1b6
z-e-a <e.a.zubkov@yandex.ru>
2024-01-19 16:19:16 +0300

    feat: solve task 'getIndexOf'

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -227,8 +227,15 @@ function isPalindrome(str) {
  *  'qwerty', 'Q'     => -1
  *  'qwerty', 'p'     => -1
  */
-function getIndexOf(/* str, letter */) {
-  throw new Error('Not implemented');
+function getIndexOf(str, letter) {
+  let result = -1;
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] === letter) {
+      result = i;
+      break;
+    }
+  }
+  return result;
 }
 
 /**
